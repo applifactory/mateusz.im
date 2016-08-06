ccd60da2c3b353b05b199ac8ae818e4c2a3eeb47
Matthew Witalinski <mateusz@applifactory.pl>
2016-08-06 22:23:08 +0200

    Add scroll effects

--- src/js/components/scroll.js
+++ src/js/components/scroll.js
@@ -1,16 +1,16 @@
 $(function(){
   $('[before-scroll], [after-scroll]').each(function(){
     var $this = $(this),
-        beforeOffset = 0,
-        afterOffset = 0,
+        beforeOffset = $this.attr('before-offset') || 0,
+        afterOffset = $this.attr('after-offset') || 0,
         clientHeight = $this[0].clientHeight,
         offsetTop = $this[0].offsetTop,
         beforeClass = $this.attr('before-scroll'),
         afterClass = $this.attr('after-scroll');
     $(window).scroll(function(){
       var scrollTop = document.body.scrollTop || document.documentElement.scrollTop,
-        isBefore = scrollTop + window.innerHeight - ( $this.offset().top + $this[0].clientHeight/2 ) < 0,
-        isAfter = scrollTop - ( $this.offset().top + $this[0].clientHeight/2 ) > 0;
+        isBefore = scrollTop + window.innerHeight - ( $this.offset().top + $this[0].clientHeight/2 ) < -beforeOffset,
+        isAfter = scrollTop - ( $this.offset().top + $this[0].clientHeight/2 ) > afterOffset;
       if ( beforeClass && beforeClass == afterClass ) {
         if ( isBefore || isAfter ) {
           $this.addClass(beforeClass);
