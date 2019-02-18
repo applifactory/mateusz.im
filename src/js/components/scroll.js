20537734b48f483fc25701edb2cad2d36e3c6781
Mateusz Witaliński <matthew.w@314t.com>
2019-02-18 03:08:02 +0100

    Add once flag to scroll effect

--- src/js/components/scroll.js
+++ src/js/components/scroll.js
@@ -6,34 +6,46 @@ $(function(){
         clientHeight = $this[0].clientHeight,
         offsetTop = $this[0].offsetTop,
         beforeClass = $this.attr('before-scroll'),
-        afterClass = $this.attr('after-scroll');
-    $(window).scroll(function(){
-      var scrollTop = document.body.scrollTop || document.documentElement.scrollTop,
-        isBefore = scrollTop + window.innerHeight - ( $this.offset().top + $this[0].clientHeight/2 ) < -beforeOffset,
-        isAfter = scrollTop - ( $this.offset().top + $this[0].clientHeight/2 ) > afterOffset;
-      if ( beforeClass && beforeClass == afterClass ) {
-        if ( isBefore || isAfter ) {
-          $this.addClass(beforeClass);
-        } else {
-          $this.removeClass(beforeClass);
-        }
-      } else {
-        if ( beforeClass ) {
-          if ( isBefore ) {
-            $this.addClass(beforeClass);
+        afterClass = $this.attr('after-scroll'),
+        beforeOnce = $this.attr('before-once') !== 'false',
+        afterOnce = $this.attr('after-once') !== 'false',
+        scrollHandler = function() {
+          var scrollTop = document.body.scrollTop || document.documentElement.scrollTop,
+            isBefore = scrollTop + window.innerHeight - ( $this.offset().top + $this[0].clientHeight/2 ) < -beforeOffset,
+            isAfter = scrollTop - ( $this.offset().top + $this[0].clientHeight/2 ) > afterOffset;
+          if ( beforeClass && beforeClass == afterClass ) {
+            if ( isBefore || isAfter ) {
+              $this.addClass(beforeClass);
+            } else {
+              $this.removeClass(beforeClass);
+              if ( beforeOnce || afterOnce ) {
+                $(window).unbind('scroll', scrollHandler);
+              }
+            }
           } else {
-            $this.removeClass(beforeClass);
+            if ( beforeClass ) {
+              if ( isBefore ) {
+                $this.addClass(beforeClass);
+              } else {
+                $this.removeClass(beforeClass);
+                if ( beforeOnce ) {
+                  $(window).unbind('scroll', scrollHandler);
+                }
+              }
+            }
+            if ( afterClass ) {
+              if ( isAfter ) {
+                $this.addClass(afterClass);
+              } else {
+                $this.removeClass(afterClass);
+                if ( afterOnce ) {
+                  $(window).unbind('scroll', scrollHandler);
+                }
+              }
+            }
           }
-        }
-        if ( afterClass ) {
-          if ( isAfter ) {
-            $this.addClass(afterClass);
-          } else {
-            $this.removeClass(afterClass);
-          }
-        }
-      }
-    });
+        };
+    $(window).bind('scroll', scrollHandler);
   });
   $(window).trigger('scroll');
   $(window).on('resize', function(){
